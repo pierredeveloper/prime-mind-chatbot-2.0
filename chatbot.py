c64a8b587479e38018e2bcefc26d137eba87e753
pierrejeandata <pierrejeanba1947@gmail.com>
2026-08-18 08:07:39 -0400

    Change LLM model from llama-3.3 to gpt-oss-20b

--- chatbot.py
+++ chatbot.py
@@ -231,7 +231,7 @@ for msg in chat_history:
 # --------------------------------------------------
 llm = ChatGroq(
     api_key=os.getenv("GROQ_API_KEY"),
-    model="llama-3.3-70b-versatile",
+    model="openai/gpt-oss-20b",
     #model="openai/gpt-oss-120b",
     temperature=0.7,
     model_kwargs={"top_p": 0.9}
